34be620f7c4c432b7969dae23389a24133b4e394
M1KUS3Q <mikolaj.popik@icloud.com>
2026-06-10 19:46:45 +0200

    fix: modify reqwest features for android compatibility; fix force upgrade

--- src/utils.rs
+++ src/utils.rs
@@ -1,13 +1,19 @@
 use self_update::cargo_crate_version;
 
 pub fn upgrade_binary(force: bool) -> anyhow::Result<()> {
+    let current_version = if force {
+        "0.0.0"
+    } else {
+        cargo_crate_version!()
+    };
+
     let status = self_update::backends::github::Update::configure()
         .repo_owner("M1KUS3Q")
         .repo_name("foody")
         .bin_name("foody")
         .show_download_progress(true)
         .no_confirm(force)
-        .current_version(cargo_crate_version!())
+        .current_version(current_version)
         .build()?
         .update()?;
 
